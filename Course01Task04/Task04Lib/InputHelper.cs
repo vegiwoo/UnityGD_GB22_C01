9ec0cd1227497d9b07b69ab83a3b5b4a0523a17d
vegiwoo <vegiwoo@protonmail.com>
2022-06-26 02:42:04 +0300

    Refactoring Course01Task04

--- Course01Task04/Task04Lib/InputHelper.cs
+++ Course01Task04/Task04Lib/InputHelper.cs
@@ -1,28 +1,28 @@
 using System;
 
-namespace Task04Lib; 
-
-public class InputHelper {
-    private static readonly InputHelper Instance = new InputHelper();
+namespace Task04Lib {
+    public class InputHelper {
+        private static readonly InputHelper Instance = new InputHelper();
     
-    public static InputHelper GetInstance() => Instance;
+        public static InputHelper GetInstance() => Instance;
     
-    /// <summary>
-    /// Ожидает дальнейшие действия пользователя после решения задачи.
-    /// </summary>
-    /// <returns>Нажатая клавиша.</returns>
-    /// <remarks>Ожидает нажания Spacebar или Backspace</remarks>>
-    public static ConsoleKey WaitUserAction()
-    {
-        ConsoleKey key = default;
-        
-        while (key != ConsoleKey.Spacebar && key != ConsoleKey.Backspace)
+        /// <summary>
+        /// Ожидает дальнейшие действия пользователя после решения задачи.
+        /// </summary>
+        /// <returns>Нажатая клавиша.</returns>
+        /// <remarks>Ожидает нажания Spacebar или Backspace</remarks>>
+        public static ConsoleKey WaitUserAction()
         {
-            Console.WriteLine($"ПОВТОРИТЬ задачу: Spacebar, ВЕРНУТЬСЯ в меню программ: Backspace");
-            key = Console.ReadKey().Key;
-        }
+            ConsoleKey key = default;
         
-        Console.Clear();
-        return key;
+            while (key != ConsoleKey.Spacebar && key != ConsoleKey.Backspace)
+            {
+                Console.WriteLine($"ПОВТОРИТЬ задачу: Spacebar, ВЕРНУТЬСЯ в меню программ: Backspace");
+                key = Console.ReadKey().Key;
+            }
+        
+            Console.Clear();
+            return key;
+        }
     }
-}
+} 
